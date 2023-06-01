b8f85240e8e1aefd9f450de50f63d1c6993e0ccf
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2023-06-01 13:34:09 +0200

    Implemented OpenSearchConsumer

--- kafka-producer-wikimedia/src/main/java/skazinski/szymon/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/skazinski/szymon/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -14,13 +14,18 @@ public class WikimediaChangesProducer {
 
     public static void main(String[] args) throws InterruptedException {
 
-        String bootstrapServers = "172.29.45.4:9092";
+        String bootstrapServers = "172.31.234.230:9092";
 
         // set producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //set high throughoutput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
